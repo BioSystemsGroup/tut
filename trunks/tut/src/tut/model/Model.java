cb7e81d00387b4e4c9a0b9eeae751593aaf02a26
gepr <gepr@8eaee825-406c-4f15-a8ec-4a324798f9e8>
2015-04-24 18:17:50 +0000

    o replicate the model in Liang and Derendorf o step back from individual Comp in and out rates to accomodate the monolithic model in L&D. o default parameters.json is Case #1. o Comps are full blown agents, executing monolithic functions given to them by the Model.  But this is irrelevant because the concentration functions are defined by the Model.  Cycle is the only variable. o Comps 0 and 1 are executed in pRNG order.  But because it's monolithic, that's irrelevant to the result.

--- trunks/tut/src/tut/model/Model.java
+++ trunks/tut/src/tut/model/Model.java
@@ -9,27 +9,56 @@
  */
 package tut.model;
 
+import java.util.function.Function;
+
 public class Model implements sim.engine.Steppable {
   public static int MODEL_ORDER = 10;
   public static int SUB_ORDER = 11;
   public ec.util.MersenneTwisterFast pRNG = null;
-  Comp central = null, periph = null;
-  public double k_a = Double.NaN, k_10 = Double.NaN, k_12 = Double.NaN, k_21 = Double.NaN;
+  public Comp central = null, periph = null;
   tut.ctrl.Parameters params = null;
+  public double cycle2time = 0.25;
   public Model(tut.ctrl.Parameters p) {
     if (p != null) params = p;
   }
   public void init(sim.engine.SimState state) {
     pRNG = state.random;
-    central = new Comp(0, 0.0, params.k_a, params.k_10);
+    calcConstants();
+    Function func = concCent();
+    central = new Comp(0, func, 0.0);
     state.schedule.scheduleOnce(central, SUB_ORDER);
-    periph = new Comp(1, 0.0, params.k_21, params.k_12);
+    func = concPeriph();
+    periph = new Comp(1, func, 0.0);
     state.schedule.scheduleOnce(periph, SUB_ORDER);
   }
 
   @Override
   public void step(sim.engine.SimState state) {
-    tut.ctrl.Batch.log("Model.step() - cycle = "+state.schedule.getSteps());
     state.schedule.scheduleOnce(this, MODEL_ORDER);
+  }
+  double A = Double.NaN, B = Double.NaN;
+  double α = Double.NaN, β = Double.NaN;
+  void calcConstants() {
+    double αβ_1 = params.k_12 + params.k_21 + params.k_10;
+    double αβ_2 = Math.sqrt(Math.pow(αβ_1, 2.0) - 4.0*params.k_21*params.k_10);
+    α = 0.5 * (αβ_1 + αβ_2);
+    β = 0.5 * (αβ_1 - αβ_2);
+    A = (params.k_a * params.dose)/params.vc * (params.k_21 - α)/((β-α)*(params.k_a-α));
+    B = (params.k_a * params.dose)/params.vc * (params.k_21 - β)/((α-β)*(params.k_a-β));
+    //tut.ctrl.Batch.log("calcConstants(): "+params.describe()+", αβ_1="+αβ_1+", αβ_2="+αβ_2+", α="+α+", β="+β+", A="+A+", B="+B);
+  }
+  Function<Double, Double> concCent() {
+    return (Double c) -> {
+      double t = c*cycle2time;
+      return A*Math.exp(-α*t) + B*Math.exp(-β*t) - (A+B)*Math.exp(-params.k_a*t);
+    };
+  }
+  Function<Double, Double> concPeriph() {
+    return (Double c) -> {
+      double t = c*cycle2time;
+      return (A*params.k_21)/(params.k_21-α) * Math.exp(-α*t)
+              + (B*params.k_21)/(params.k_21-β) * Math.exp(-β*t)
+              + ((B-A)*params.k_21)/(params.k_21-params.k_a) * Math.exp(-params.k_a*t);
+    };
   }
 }
