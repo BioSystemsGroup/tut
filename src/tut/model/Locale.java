4764e64c17cd784f8725944e5d8206e1efe446b3
gepr <gepr@8eaee825-406c-4f15-a8ec-4a324798f9e8>
2015-06-17 00:25:12 +0000

    o Added separate rate constants for Marker   - had to restructure parameters and the way the network is specified   - Replaced Gson with Genson for the Json binding becuase Gson does not handle nested objects by default.   - Had to override init() as well as instantiate() in LooseDyn in order to accommodate separate Drug and Marker rate constants.

--- src/tut/model/Locale.java
+++ src/tut/model/Locale.java
@@ -12,8 +12,8 @@ package tut.model;
 import java.util.Map;
 
 public class Locale extends Comp {
-  Map<? extends Locale,Double> ins = null;
-  public void setIns(Map<? extends Locale,Double> inl) { ins = inl; }
+  Map<? extends Locale,Map<String,Double>> ins = null;
+  public void setIns(Map<? extends Locale,Map<String,Double>> inl) { ins = inl; }
   double volume = Double.NaN;
   
   public Locale(int i, double start, double v) {
@@ -27,14 +27,16 @@ public class Locale extends Comp {
     
     if (ins == null) return;  // null inputs = source
     
-    for (Map.Entry<? extends Locale,Double> cme : ins.entrySet()) {
+    for (Map.Entry<? extends Locale,Map<String,Double>> cme : ins.entrySet()) {
       Locale inComp = cme.getKey();
-      double inRate = cme.getValue();
+      Map<String,Double> rateMap = cme.getValue();
       for (Map.Entry<String,sim.util.MutableDouble> sme : particles.entrySet()) {
-        sim.util.MutableDouble inSpecies = inComp.particles.get(sme.getKey());
-        double inc = inSpecies.val*inRate;
-        inSpecies.val -= inc; // decrement theirs
-        particles.get(sme.getKey()).val += inc; // increment mine
+        String inParticleName = sme.getKey();
+        sim.util.MutableDouble inParticleAmount = inComp.particles.get(inParticleName);
+        double inRate = rateMap.get(inParticleName);
+        double inc = inParticleAmount.val*inRate;
+        inParticleAmount.val -= inc; // decrement theirs
+        particles.get(inParticleName).val += inc; // increment mine
       }
     }
   }
