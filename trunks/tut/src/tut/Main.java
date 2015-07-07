4adc69a4125348535ffa10d530483164f9a889d1
gepr <gepr@8eaee825-406c-4f15-a8ec-4a324798f9e8>
2015-07-07 19:10:32 +0000

    o bring case-?? files into accordance with parameters.json o bump version to 0.7 o change back from fraction of dose to amount/concentration

--- trunks/tut/src/tut/Main.java
+++ trunks/tut/src/tut/Main.java
@@ -10,7 +10,7 @@
 package tut;
 
 public class Main {
-  public static final String MAJOR_VERSION = "TUT-v0.6";
+  public static final String MAJOR_VERSION = "TUT-v0.7";
   public static final String MINOR_VERSION = "$Revision$";
   public static void main(String[] args) {
     // run the GUI?
